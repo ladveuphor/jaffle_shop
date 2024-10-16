a9623654d78fd76ce51a4a89cdad9f015af50ab9
ladveuphor <laurent.dangvu@euphor.fr>
2024-10-16 21:45:07 +0200

    Update tests ephemeral model avec plusieurs parametres et plusieurs with (CTE) + appel depuis table model

--- models/staging/jaffle_shop/ephemeral_model_test.sql
+++ models/staging/jaffle_shop/ephemeral_model_test.sql
@@ -7,6 +7,16 @@
 {% set input_table = var('input_table', 'customers') %} 
 {% set column_name = var('column_name', 'FIRST_NAME') %} 
 
-select
-    "{{ column_name }}"
-from {{ source('jaffle_shop', input_table) }}
+with 
+BBB as(
+  select
+      "{{ column_name }}"
+  from {{ source('jaffle_shop', input_table) }}
+),
+
+AAA as(
+  select * from BBB
+)
+
+select * from AAA
+
